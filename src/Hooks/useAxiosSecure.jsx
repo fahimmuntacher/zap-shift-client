37bab2283b2b8915191cba38907783e7ad52e4d1
fahimmuntacher <muntacherfahim234@gmail.com>
2025-11-22 16:03:32 +0600

    jwt verification done

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,19 +1,44 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import useAuth from './useAuth';
+import axios from "axios";
+import React, { useContext, useEffect } from "react";
+import useAuth from "./useAuth";
+import { AuthContext } from "../Context/AuthContext/AuthContext";
+import { useNavigate } from "react-router";
 const instance = axios.create({
-    baseURL: "http://localhost:3000"
-})
+  baseURL: "http://localhost:3000",
+});
 const useAxiosSecure = () => {
-    const {user} = useAuth()
-    useEffect(() =>{
-        instance.interceptors.request.use(config => {
-            config.headers.authorization = `Bearer ${user?.accessToken}`
-            return config
-        })
-    }, [user])
-    return instance;
+  const { user } = useAuth();
+  const { signOutUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const requestIntercepts = instance.interceptors.request.use((config) => {
+      config.headers.authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
 
+    // response intercepts
+    const responseInterceptor = instance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (err) => {
+        console.log(err);
+        const statusCode = err.status;
+        if (statusCode === 401 || statusCode === 403) {
+          signOutUser().then(() => {
+            navigate("/login");
+          });
+        }
+        return Promise.reject(err);
+      }
+    );
+
+    return () => {
+      axios.interceptors.request.eject(requestIntercepts);
+      axios.interceptors.response.eject(responseInterceptor);
+    };
+  }, [user, navigate, signOutUser]);
+  return instance;
 };
 
 export default useAxiosSecure;
