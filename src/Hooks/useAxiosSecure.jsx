33a2d7dfd949a7553bd64943dab346f8ff5a2fe6
fahimmuntacher <muntacherfahim234@gmail.com>
2025-11-22 12:15:01 +0600

    headers accesstoken added

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,19 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
+import useAuth from './useAuth';
 const instance = axios.create({
     baseURL: "http://localhost:3000"
 })
 const useAxiosSecure = () => {
+    const {user} = useAuth()
+    useEffect(() =>{
+        instance.interceptors.request.use(config => {
+            config.headers.authorization = `Bearer ${user?.accessToken}`
+            return config
+        })
+    }, [user])
     return instance;
+
 };
 
 export default useAxiosSecure;
